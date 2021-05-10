cd56aead12a6c5fb8c8d27765484cf397441c9f1
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-05-10 13:02:44 +0430

    Updates

---
+++ precache-manifest.5fe4fd59102953a43d49ac44778d1950.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed2f368c23c1f9e040d50000c20ffa6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70e51d15a2a261b4e43d",
+    "url": "./static/css/2.0b5d44dd.chunk.css"
+  },
+  {
+    "revision": "70e51d15a2a261b4e43d",
+    "url": "./static/js/2.7eb91063.chunk.js"
+  },
+  {
+    "revision": "c28c828472ec79fe587c70dcc85af128",
+    "url": "./static/js/2.7eb91063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e188951c2c5e10296a1",
+    "url": "./static/js/main.c58a01c2.chunk.js"
+  },
+  {
+    "revision": "5e351d6cd74a5b72bd10",
+    "url": "./static/js/runtime-main.9d716fe2.js"
+  },
+  {
+    "revision": "063a5ea0cf2d01106c068460fda0314f",
+    "url": "./static/media/index.063a5ea0.less"
+  },
+  {
+    "revision": "38a866cfd1e408e013c7400242d87a58",
+    "url": "./static/media/index.38a866cf.less"
+  },
+  {
+    "revision": "855209dd8a033c606a1554aa245dfb4d",
+    "url": "./static/media/index.855209dd.less"
+  },
+  {
+    "revision": "af29c867a8c746f94bf9d06765ad7af2",
+    "url": "./static/media/index.af29c867.less"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "./static/media/index.c17ec543.less"
+  },
+  {
+    "revision": "e5c4097ae3bc1fd6730b519e0798afec",
+    "url": "./static/media/index.e5c4097a.less"
+  },
+  {
+    "revision": "f13ee1cedadf662506389fd8560f9990",
+    "url": "./static/media/index.f13ee1ce.less"
+  },
+  {
+    "revision": "f35d4d540caff457e15b81967d87da72",
+    "url": "./static/media/index.f35d4d54.less"
+  }
+]);
