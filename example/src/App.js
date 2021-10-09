fffe2a7ac86f12fdc8e2ec84b2f3c57284048d2d
Matin Rezakhani <matin.rezakhani@gmail.com>
2021-10-09 12:13:10 +0330

    update home page

--- example/src/App.js
+++ example/src/App.js
@@ -16,9 +16,23 @@ const App = () => {
 
   return (
     <div>
+      <Row justify={'space-between'} className={'header'} align={'middle'}>
+          <Col sm={10} className={'header'}>
+            <Typography className={'title'}>Datepicker Jalali React</Typography>
+          </Col>
+          <Col>
+              <a href={'https://www.npmjs.com/package/datepicker-jalali-react'} target={'_blank '} className={'install'}>Install</a>
+              <a href={'https://github.com/matinrezakhani/datepicker-jalali-react'} target={'_blank '} className={'install'}>Github</a>
+          </Col>
+      </Row>
       <Row justify={'center'}>
         <Col xs={{ span: 23 }} md={{ span: 10 }} className={'card'}>
-          <Row justify={'space-around'} align={'middle'}>
+          <Row justify={'center'} align={'middle'}>
+            <Col className={'title'} style={{marginRight: 10}}>
+                <Text>
+                  Date and Time:
+                </Text>
+            </Col>
             <Col className={'title'}>
               {date && (
                 <Text>
@@ -42,7 +56,12 @@ const App = () => {
       </Row>
       <Row justify={'center'}>
         <Col xs={{ span: 23 }} md={{ span: 10 }} className={'card'}>
-          <Row justify={'space-around'} align={'middle'}>
+          <Row justify={'center'} align={'middle'}>
+            <Col className={'title'} style={{marginRight: 10}}>
+                <Text>
+                  Date:
+                </Text>
+            </Col>
             <Col className={'title'}>
               {date && (
                 <Text>
@@ -67,7 +86,12 @@ const App = () => {
       </Row>
       <Row justify={'center'}>
         <Col xs={{ span: 23 }} md={{ span: 10 }} className={'card'}>
-          <Row justify={'space-around'} align={'middle'}>
+          <Row justify={'center'} align={'middle'}>
+            <Col className={'title'} style={{marginRight: 10}}>
+                <Text>
+                   Time: 
+                </Text>
+            </Col>
             <Col className={'title'}>
               {date && (
                 <Text>
